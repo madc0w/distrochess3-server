ac99121604adb7c22889339af9fb9f94c1b1c5c3
Marc A. Donis <marc@balloonary.io>
2025-12-15 12:23:37 +0100

    .

--- i18n/en.ts
+++ i18n/en.ts
@@ -3,7 +3,7 @@ export const en = {
 		autoResign: {
 			subject: 'Your DistroChess game will auto-resign soon!',
 			greeting: 'Hi {name},',
-			body: "If you (or another player on your side) doesn't make a move within the next {hours} hours, your side will forfeit the game. This means your side will be resigning, and you will lose points, resulting in sad ducks.",
+			body: "If you (or one of your teammates) don't make a move within the next {hours} hours, your side will forfeit the game. This means your side will be resigning, and you will lose points, resulting in sad ducks.",
 			description:
 				'Click below to jump back into the game before the auto-resign timer expires.',
 			ctaText: 'Make your move',
